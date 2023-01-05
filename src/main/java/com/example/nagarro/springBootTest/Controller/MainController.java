965c2b698b449d23a0f75189adecc5b5d7181261
Mohammad Ansari <mohammad.ansari01@nagarro.com>
2023-01-05 14:14:16 +0530

    test list added at /list

--- src/main/java/com/example/nagarro/springBootTest/Controller/MainController.java
+++ src/main/java/com/example/nagarro/springBootTest/Controller/MainController.java
@@ -3,11 +3,20 @@ package com.example.nagarro.springBootTest.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class MainController {
 
     @GetMapping("/test")
     public String test(){
         return "Rest API is working successfully!.........";
+    }
+
+    @GetMapping("/list")
+    public List getNumbers(){
+        List<String> arrayList = List.of("one","two", "three", "four", "five");
+        return arrayList;
     }
 }
